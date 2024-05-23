ed691a91ecedcafd11a884744a86a447192a16b4
Artym <Starvars9@mail.ru>
2024-05-23 13:09:09 +0300

    1

---
+++ Dump_marketplace_for_the_sale_of_advertising/marketplace_for_the_sale_of_advertising_rbac_permissions.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: marketplace_for_the_sale_of_advertising
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `rbac_permissions`
+--
+
+DROP TABLE IF EXISTS `rbac_permissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rbac_permissions` (
+  `PermissionID` int NOT NULL AUTO_INCREMENT,
+  `PermissionName` varchar(100) NOT NULL,
+  `Description` text,
+  PRIMARY KEY (`PermissionID`),
+  UNIQUE KEY `PermissionName` (`PermissionName`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rbac_permissions`
+--
+
+LOCK TABLES `rbac_permissions` WRITE;
+/*!40000 ALTER TABLE `rbac_permissions` DISABLE KEYS */;
+INSERT INTO `rbac_permissions` VALUES (1,'create_ad','Create advertising space'),(2,'edit_ad','Edit advertising space'),(3,'delete_ad','Delete advertising space'),(4,'view_ad','View advertising space'),(5,'place_order','Place an order'),(6,'view_order','View order'),(7,'manage_users','Manage other users'),(8,'review_ad','Review advertising space'),(9,'process_transactions','Process financial transactions');
+/*!40000 ALTER TABLE `rbac_permissions` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-23 13:08:21
